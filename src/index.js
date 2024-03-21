ce36a8696cc68a0ec382efc26525aead91a0aa50
ritik <gupta.ritik2002@gmail.com>
2024-03-21 14:16:36 +0530

    basic server setup done

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT } = require("./config/serverConfig");
+
+// const apiRoutes = require("./routes/index.js");
+const app = express();
+const prepareAndStartServer = async () => {
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+//   app.use("/api", apiRoutes);
+  app.listen(PORT, async () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+};
+
+prepareAndStartServer();
