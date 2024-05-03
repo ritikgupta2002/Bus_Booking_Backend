7ebab469387b35687a471aab9ebe05e74cf53a35
ritik <gupta.ritik2002@gmail.com>
2024-05-03 16:06:54 +0530

    added validateIsAdminRequest middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,3 +1,4 @@
 module.exports={
     AuthRequestValidators:require('./auth-request-validator.js'),
+    validateIsAdminRequest:require("./auth-request-validator.js"),
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const UserController = require("../../controllers/user-controller");
 const router=express.Router();
-const {AuthRequestValidators}=require("../../middlewares/index.js");
+const {AuthRequestValidators,ValidateIsAdminRequest}=require("../../middlewares/index.js");
 
 router.post("/signup",AuthRequestValidators.validateUserAuth,UserController.create);
 router.post("/signin",AuthRequestValidators.validateUserAuth,UserController.signIn);
 router.get("/isAuthenticated",UserController.isAuthenticated);
+router.get("/isAdmin",AuthRequestValidators.validateIsAdminRequest,UserController.isAdmin);
 
 
 module.exports=router;
