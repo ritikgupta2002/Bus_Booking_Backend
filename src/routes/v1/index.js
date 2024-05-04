5d2729193ff68afbe430ced4407367ac4db42cb8
ritik <gupta.ritik2002@gmail.com>
2024-05-04 18:16:55 +0530

    added rate limiting logic for securing signup , signin  , verifyEmail apis from brute force attacks

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const UserController = require("../../controllers/user-controller");
 const router = express.Router();
 const {
@@ -6,22 +7,44 @@ const {
   ValidateIsAdminRequest,
 } = require("../../middlewares/index.js");
 
+const signupLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: "Too many requests to /signup, please try again after 5 mins",
+});
+
+const signinLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: "Too many requests to /signin, please try again after 5 mins",
+});
+
+const verifyEmailLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: "Too many requests to /verifyEmail, please try again after 5 mins",
+});
+
 router.post(
   "/signup",
+  signupLimiter,
   AuthRequestValidators.validateUserAuth,
   UserController.register
 );
 router.post(
   "/signin",
+  signinLimiter,
   AuthRequestValidators.validateUserAuth,
   UserController.signIn
 );
+router.post("/verifyEmail", verifyEmailLimiter, UserController.verifyEmail);
+
 router.get("/isAuthenticated", UserController.isAuthenticated);
+
 router.get(
   "/isAdmin",
   AuthRequestValidators.validateIsAdminRequest,
   UserController.isAdmin
 );
-router.post("/verifyEmail", UserController.verifyEmail);
 
 module.exports = router;
