132dfdb93739c1c9698f2bad307e9636b77e3490
ritik <gupta.ritik2002@gmail.com>
2024-05-14 15:28:31 +0530

    setup of smtp using nodemail done

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-const{PORT}=require("./config/serverConfig");
+const { PORT } = require("./config/serverConfig");
+
+const {sendBasicEmail} = require("./services/email-service");
 
 const setupAndStartServer = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
+   sendBasicEmail(
+      "support@admin.com",
+      "reminderservice2002@gmail.com",
+      "testing mail ",
+      "this is a testing mail "
+    );
   });
 };
 
